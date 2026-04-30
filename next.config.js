87a4cdd8d0731fabcd53aa5679b360527a650a8e
Dharamveer <102931119+dharamveerkz@users.noreply.github.com>
2026-04-30 22:01:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Optional: Add custom headers for security
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ]
+      }
+    ];
+  }
 }
+
 module.exports = nextConfig
